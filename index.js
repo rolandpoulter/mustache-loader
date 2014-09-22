a6cc83a9e8260698fdb7421d05f80ce272adf12d
Roland Poulter <rolandpoulter@gmail.com>
2014-09-21 17:15:16 -0700

    Update index.js

--- index.js
+++ index.js
@@ -7,11 +7,5 @@ module.exports = function(source) {
         this.cacheable();
     }
 
-    return 'var H = require("hogan.js");\n' +
-           'module.exports = function() { ' +
-           'var T = new H.Template(' +
-           Hogan.compile(source, { asString: true }) +
-           ', ' +
-           JSON.stringify(source) +
-           ', H); return T.render.apply(T, arguments); };';
+    return 'module.exports = ' + Hogan.compile(source).toString() + ';';
 };
